ba843425bb7ef2af4988c1f62c2701958d7d5896
racoonkingtop1 <pavluxa_2005@mail.ru>
2023-04-24 22:53:31 +0500

    new section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let body = document.querySelector(`#body`);
 let burgerBtn = document.querySelector(`.burger__btn`);
 let burgerBtnImg = document.querySelector(`burger__btn__img`)
 let burgerMenu = document.querySelector(`.header__burger`);
@@ -9,5 +10,12 @@ burgerBtn.addEventListener(`click`, function(){
 document.addEventListener(`click`, function(evt){
     if (!(evt.target === burgerMenu) && !(evt.target.classList.contains(`burger__btn__img`))){
         burgerMenu.classList.add(`w0`)
+    }
+    
+    if (!(burgerMenu.classList.contains(`w0`))) {
+        body.classList.add(`body_scrollbar_none`);
+        console.log(`f`)
+    } else {
+        body.classList.remove(`body_scrollbar_none`)
     }
 });
